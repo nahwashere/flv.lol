ed9c1f25a0c006e754f83714df7dbcc8d218016f
Nah <146902470+nahwashere@users.noreply.github.com>
2023-10-04 13:59:22 +0200

    Delete assets directory

--- assets/js/typed.js
+++
@@ -1,9 +0,0 @@
-new Typed('#typed', {
-    strings: ['I suck MrBeasts toes'],
-    typeSpeed: 120,
-    backSpeed: 120,
-    backDelay: 1000,
-    startDelay: 120,
-    loop: true,
-    showCursor: false
-});
