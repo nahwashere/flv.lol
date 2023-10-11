510a7b9b1ffa77132be494d244ae3b110ae42a79
Nah <146902470+nahwashere@users.noreply.github.com>
2023-10-11 17:07:17 +0200

    Delete penis directory

--- penis/assets/js/typed.js
+++
@@ -1,9 +0,0 @@
-new Typed('#typed', {
-    strings: ['flv was here'],
-    typeSpeed: 120,
-    backSpeed: 120,
-    backDelay: 1000,
-    startDelay: 120,
-    loop: true,
-    showCursor: false
-});
